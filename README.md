34729ef0b0bbe188075b9e930bca8e69e654859f
yeliex <yeliex@yeliex.com>
2016-05-18 15:59:37 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # node.date-time
+
+[![npm](https://img.shields.io/npm/v/node.date-time.svg?style=flat-square)](https://www.npmjs.com/package/node.date-time)
+
 date-time formatter for javascript
+
+## Installation
+```
+$ npm install node.date-time
+```
+
+## Use with CMD
+```js
+const format = require("node.date-time");
+console.log(new Date().format("Y-M-d")); // 2016-5-18
+console.log(format("y-M-d H:m:s")); // 16-5-18 15:45:8
+```
+
+## Use with html
+```js
+<script src="./node_modules/node.date-time/index.js"></script>
+<script>
+console.log(format("Y-MM-dd HH:mm:ss", new Date())); // 2016-05-03 01:01:01
+</script>
+```
+
+## Format
+
+- `yyyy`,`Y`
+    - A full numeric representation of a year
+    - eg: 2016,1998
+- `yy`,`y`
+    - A two digit representation of a year
+    - 16,98
+- `MM`
+    - A two digit representation of a month
+    - 01-12
+- `M`
+    - A month
+    - 1-12
+- `dd`
+    - A two digit representation of a date
+    - 01-31
+- `d`
+    - A date
+    - 1-31
+- `D`
+    - A day in a week
+    - 0-6
+- `HH`,`hh`
+    - A two digit representation of a hout
+    - 00-23
+- `H`,`h`
+    - An hour
+    - 0-23
+- `mm`
+    - A two digit representation of a minute
+    - 00-59
+- `m`
+    - A minute
+    - 0-59
+- `ss`
+    - A two digit representation of a second
+    - 00-59
+- `s`
+    - A second
+    - 0-59
+- `ms`
+    - A microsecond
+    - 000-999
---
+++ index.js
@@ -0,0 +1,36 @@
+/**
+ * Created by yeliex on 16/5/18.
+ */
+const leftpad = require("left-pad");
+(function () {
+    const formatter = function (rule, date = (new Date())) {
+        return (
+            rule
+                .replace(/yyyy|Y/g, date.getFullYear())
+                .replace(/yy|y/g, String(date.getFullYear()).substr(-2, 2))
+                .replace(/MM/g, leftpad(String(date.getMonth() + 1), 2, "0"))
+                .replace(/M/g, date.getMonth() + 1)
+                .replace(/dd/g, leftpad(String(date.getDate()), 2, "0"))
+                .replace(/d/g, date.getDate())
+                .replace(/D/g, date.getDay())
+                .replace(/HH|hh/g, leftpad(String(date.getHours()), 2, "0"))
+                .replace(/H|h/g, date.getHours())
+                .replace(/ms/g, date.getMilliseconds())
+                .replace(/mm/g, leftpad(String(date.getMinutes()), 2, "0"))
+                .replace(/m/g, leftpad(String(date.getMinutes()), 3, "0"))
+                .replace(/ss/g, leftpad(String(date.getSeconds()), 2, "0"))
+                .replace(/s/g, date.getSeconds())
+        );
+    };
+    Date.prototype.format = function (rule) {
+        return formatter(rule, this);
+    };
+
+    if (typeof exports === "object") {
+        module.exports = formatter;
+    } else {
+        this.format = formatter;
+    }
+
+    console.log(formatter("y-M-d H:m:s"))
+}());
